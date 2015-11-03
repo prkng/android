c90c1118ca7e80775233ac07844fe82ea2070d33
mudar <mn@mudar.ca>
2015-11-03 17:36:51 -0500

    Display mapbox mapview
    tests with MapFragment not worth it

--- prkng-project/prkng/src/main/java/ng/prk/prkngandroid/MainActivity.java
+++ prkng-project/prkng/src/main/java/ng/prk/prkngandroid/MainActivity.java
@@ -3,6 +3,7 @@ package ng.prk.prkngandroid;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,30 +11,28 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.crashlytics.android.Crashlytics;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
+
 import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener,
+        MapView.OnMapChangedListener {
+    private final static String TAG = MainActivity.class.getSimpleName();
 
     private MapView mapView;
 
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        mapView.onStart();
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, new Crashlytics());
+        }
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -56,19 +55,13 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        createMap(savedInstanceState);
+        createMapIfNecessary(savedInstanceState);
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
-
-    @Override
-    public void onPause()  {
-        super.onPause();
-        mapView.onPause();
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
     }
 
     @Override
@@ -78,9 +71,27 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 
     @Override
@@ -117,15 +128,23 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    private void createMap(Bundle savedInstanceState) {
+    private void createMapIfNecessary(Bundle savedInstanceState) {
+        if (mapView == null) {
+            mapView = (MapView) findViewById(R.id.mapview);
+            mapView.setCenterCoordinate(new LatLng(45.501689, -73.567256));
+            mapView.setZoomLevel(14);
+            mapView.onCreate(savedInstanceState);
 
-        mapView = (MapView) findViewById(R.id.mapview);
-//        sk.eyJ1IjoibXVkYXIiLCJhIjoiY2lnaWhoamhoMDAwM3VtbHoxcnF6MndxciJ9.S8y33ms4iSmJD8CHW_0e_g
-        mapView.setAccessToken("pk.eyJ1IjoiYXJuYXVkc3B1aGxlciIsImEiOiJSaEctSlVnIn0.R8cfngN9KkHYZx54JQdgJA");
-        mapView.setStyleUrl("mapbox://styles/arnaudspuhler/cigfc11g0000dcnm73r4o3pi1");
-//        mapView.setStyle(Map);
-        mapView.setCenterCoordinate(new LatLng(45.501689, -73.567256));
-        mapView.setZoomLevel(14);
-        mapView.onCreate(savedInstanceState);
+            mapView.addOnMapChangedListener(this);
+        }
+    }
+
+    @Override
+    public void onMapChanged(int change) {
+        Log.v(TAG, "onMapChanged @ " + change);
+        if (change == MapView.DID_FINISH_RENDERING_FRAME_FULLY_RENDERED) {
+            Snackbar.make(mapView, "Map region DID change", Snackbar.LENGTH_SHORT)
+                    .setAction("Action", null).show();
+        }
     }
 }
