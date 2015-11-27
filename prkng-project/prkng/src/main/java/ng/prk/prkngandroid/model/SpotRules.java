7ad4c19fa795d6cd1c385da2c48699f310b6d13a
Mudar Noufal <mn@mudar.ca>
2015-11-27 10:40:44 -0500

    Agenda intervals

--- prkng-project/prkng/src/main/java/ng/prk/prkngandroid/model/SpotRules.java
+++ prkng-project/prkng/src/main/java/ng/prk/prkngandroid/model/SpotRules.java
@@ -1,8 +1,6 @@
 package ng.prk.prkngandroid.model;
 
 
-import android.util.Log;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -15,6 +13,8 @@ import ng.prk.prkngandroid.util.CalendarUtils;
 
 public class SpotRules {
     private final static String TAG = "SpotRules";
+    private final static int INDEX_START = 0;
+    private final static int INDEX_END = 1;
 
     private List<SpotRule> rules;
 
@@ -42,113 +42,190 @@ public class SpotRules {
         return null;
     }
 
-    public void getParkingAgenda() {
+    public List<RestrInterval> getParkingAgenda() {
         int today = CalendarUtils.getIsoDayOfWeek();
 
-        final Map<Integer, List<ParkingPeriod>> weekStates = new HashMap<>();
+        final Map<Integer, IntervalsList> dailyIntervals = getDailyIntervalsList(rules, today);
+//        final Map<Integer, List<RestrInterval>> dailyRestrMap = getDailyRestrMap(rules, today);
+
+//        final List<ParkingRestrPeriod> mergedDailyStates = new ArrayList<>();
+//        for (int i = 1; i <= CalendarUtils.WEEK_IN_DAYS; i++) {
+//            List<ParkingRestrPeriod> dayStates = dailyRestrMap.get(i);
+//            if (dayStates.isEmpty()) {
+//                dayStates.add(new ParkingRestrPeriod(i, Const.ParkingRestrType.NONE));
+//            }
+//            Collections.sort(dayStates);
+//            mergedDailyStates.addAll(dayStates);
+//        }
+
+//        return mergeSimilarDailyRestr(dailyRestrMap, today);
+        return getIntervalsList(dailyIntervals, today);
+    }
+
+    /**
+     * Group restrictions by day, unsorted and unmerged
+     *
+     * @param rules
+     * @return
+     */
+    @Deprecated
+    private static Map<Integer, List<RestrInterval>> getDailyRestrMap(List<SpotRule> rules, int today) {
+
+        final Map<Integer, List<RestrInterval>> daysMap = new HashMap<>();
+        // Loop over each rule
         for (SpotRule rule : rules) {
             final SpotRuleAgenda agenda = rule.getAgenda();
-            for (int i = CalendarUtils.FIRST_WEEK_IN_DAY; i <= CalendarUtils.WEEK_IN_DAYS; i++) {
-                List<List<Float>> state = agenda.getDay(CalendarUtils.getIsoDayOfWeekLooped(i, today));
+            // Loop over days, for current rule
+            for (int i = 1; i <= CalendarUtils.WEEK_IN_DAYS; i++) {
+                final int dayOfWeek = CalendarUtils.getIsoDayOfWeekLooped(i, today);
+                List<List<Float>> state = agenda.getDay(dayOfWeek);
 
-                List<ParkingPeriod> dayStates = weekStates.get(i);
-                if (dayStates == null) {
-                    dayStates = new ArrayList<>();
-                    weekStates.put(i, dayStates);
+                List<RestrInterval> restrList = daysMap.get(i);
+                if (restrList == null) {
+                    restrList = new ArrayList<>();
+                    daysMap.put(i, restrList);
                 }
 
-                if (state == null || state.isEmpty() || state.get(0) == null) {
-                    continue;
-                }
-
+                int type;
                 if (rule.isTypeTimeMaxPaid()) {
-                    dayStates.add(new ParkingPeriod(
-                                    state.get(0).get(0),
-                                    state.get(0).get(1),
-                                    Const.ParkingRestrictionType.TIME_MAX_PAID)
-                    );
+                    type = Const.ParkingRestrType.TIME_MAX_PAID;
                 } else if (rule.isTypeTimeMax()) {
-                    dayStates.add(new ParkingPeriod(
-                                    state.get(0).get(0),
-                                    state.get(0).get(1),
-                                    Const.ParkingRestrictionType.TIME_MAX)
-                    );
+                    type = Const.ParkingRestrType.TIME_MAX;
                 } else if (rule.isTypePaid()) {
-                    dayStates.add(new ParkingPeriod(
-                                    state.get(0).get(0),
-                                    state.get(0).get(1),
-                                    Const.ParkingRestrictionType.PAID)
-                    );
+                    type = Const.ParkingRestrType.PAID;
                 } else {
-                    dayStates.add(new ParkingPeriod(
-                                    state.get(0).get(0),
-                                    state.get(0).get(1),
-                                    Const.ParkingRestrictionType.FORBIDDEN)
+                    type = Const.ParkingRestrType.ALL_TIMES;
+                }
+                // Loop over periods, for current rule and dayOfWeek
+                for (List<Float> restriction : state) {
+                    restrList.add(
+                            new RestrInterval(dayOfWeek,
+                                    restriction.get(INDEX_START),
+                                    restriction.get(INDEX_END),
+                                    type)
                     );
                 }
             }
         }
 
-        for (int i = CalendarUtils.FIRST_WEEK_IN_DAY; i <= CalendarUtils.WEEK_IN_DAYS; i++) {
-            List<ParkingPeriod> dayStates = weekStates.get(i);
-            Collections.sort(dayStates);
+        return daysMap;
+    }
 
-            for (ParkingPeriod state : dayStates) {
-                Log.v(TAG, state.toString());
+    private static Map<Integer, IntervalsList> getDailyIntervalsList(List<SpotRule> rules, int today) {
+
+        final Map<Integer, IntervalsList> daysMap = new HashMap<>();
+        // Loop over each rule
+        for (SpotRule rule : rules) {
+            final SpotRuleAgenda agenda = rule.getAgenda();
+            // Loop over days, for current rule
+            for (int i = 1; i <= CalendarUtils.WEEK_IN_DAYS; i++) {
+                final int dayOfWeek = CalendarUtils.getIsoDayOfWeekLooped(i, today);
+                List<List<Float>> state = agenda.getDay(dayOfWeek);
+
+                IntervalsList restrList = daysMap.get(i);
+                if (restrList == null) {
+                    restrList = new IntervalsList();
+                    daysMap.put(i, restrList);
+                }
+
+                int type;
+                if (rule.isTypeTimeMaxPaid()) {
+                    type = Const.ParkingRestrType.TIME_MAX_PAID;
+                } else if (rule.isTypeTimeMax()) {
+                    type = Const.ParkingRestrType.TIME_MAX;
+                } else if (rule.isTypePaid()) {
+                    type = Const.ParkingRestrType.PAID;
+                } else {
+                    type = Const.ParkingRestrType.ALL_TIMES;
+                }
+                // Loop over periods, for current rule and dayOfWeek
+                for (List<Float> restriction : state) {
+                    restrList.add(
+                            new RestrInterval(dayOfWeek,
+                                    restriction.get(INDEX_START),
+                                    restriction.get(INDEX_END),
+                                    type)
+                    );
+                }
             }
         }
+
+        return daysMap;
+    }
+
+    /**
+     * Merge similar restriction periods (for each day) into a single large period.
+     * Also adds a 24hrs no-restriction for empty days.
+     *
+     * @param daysMap
+     * @return
+     */
+    @Deprecated
+    private static List<RestrInterval> mergeSimilarDailyRestr(Map<Integer, List<RestrInterval>> daysMap, int today) {
+
+        final List<RestrInterval> mergedDaysMap = new ArrayList<>();
+        // Loop over the week days. Using index (1-7) to get the HashMap in ascending days order
+        for (int i = 1; i <= CalendarUtils.WEEK_IN_DAYS; i++) {
+            List<RestrInterval> restrList = daysMap.get(i);
+            if (restrList.isEmpty()) {
+                // Add a 24hrs no-restriction rule
+                restrList.add(new RestrInterval(
+                        CalendarUtils.getIsoDayOfWeekLooped(i, today),
+                        Const.ParkingRestrType.NONE));
+            } else {
+                Collections.sort(restrList);
+
+                List<RestrInterval> restrListMerged = new ArrayList<>();
+                // Loop over the day's restrictions, to merge similar intersecting one
+//                while (!restrList.isEmpty()) {
+//                    Log.v(TAG, "remove");
+//                    ParkingRestrPeriod restrCurrent = restrList.remove(0);
+//                    Log.v(TAG, "loop");
+//                    for (ParkingRestrPeriod restr : restrList) {
+//                        Log.v(TAG, "aa ");
+//                        if (restrCurrent.abuts(restr)) {
+//                            Log.v(TAG, "abuts ");
+//
+//                        }
+//                    }
+//                }
+            }
+
+
+            // Add the day's clean list to the week's merged array
+            mergedDaysMap.addAll(restrList);
+        }
+
+        return mergedDaysMap;
+    }
+
+    private static List<RestrInterval> getIntervalsList(Map<Integer, IntervalsList> daysMap, int today) {
+
+        final List<RestrInterval> mergedList = new ArrayList<>();
+        // Loop over the week days. Using index (1-7) to get the HashMap in ascending days order
+        for (int i = 1; i <= CalendarUtils.WEEK_IN_DAYS; i++) {
+            List<RestrInterval> restrList = daysMap.get(i);
+            if (restrList.isEmpty()) {
+                // Add a 24hrs no-restriction rule
+                restrList.add(new RestrInterval(
+                        CalendarUtils.getIsoDayOfWeekLooped(i, today),
+                        Const.ParkingRestrType.NONE));
+            } else {
+                Collections.sort(restrList);
+            }
+
+            // Add the day's clean list to the week's merged array
+            mergedList.addAll(restrList);
+        }
+
+        return mergedList;
     }
 
     @Override
     public String toString() {
-        getParkingAgenda();
+//        getParkingAgenda();
         return "SpotRules{ size : " + getSize() +
                 ", codes : " + getAllCodes() +
                 " }";
     }
-
-    public static class ParkingPeriod implements Comparable<ParkingPeriod> {
-        int minuteStart;
-        int minuteEnd;
-        int type;
-
-        public ParkingPeriod(float hourStart, float hourEnd, int type) {
-            this.minuteStart = (int) (hourStart * CalendarUtils.HOUR_IN_MINUTES);
-            this.minuteEnd = (int) (hourEnd * CalendarUtils.HOUR_IN_MINUTES);
-            this.type = type;
-        }
-
-        public float getMinuteStart() {
-            return minuteStart;
-        }
-
-        public float getMinuteEnd() {
-            return minuteEnd;
-        }
-
-        public int getType() {
-            return type;
-        }
-
-        @Override
-        public String toString() {
-            return "ParkingPeriod{" +
-                    "type=" + type +
-                    ", minuteStart=" + String.format("%.2f", (float) (minuteStart / CalendarUtils.HOUR_IN_MINUTES)) +
-                    ", minuteEnd=" + String.format("%.2f", (float) (minuteEnd / CalendarUtils.HOUR_IN_MINUTES)) +
-                    '}';
-        }
-
-        @Override
-        public int compareTo(ParkingPeriod another) {
-            if (getMinuteStart() < another.getMinuteStart()) {
-                return -1;
-            } else if (getMinuteEnd() > another.getMinuteEnd()) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-    }
-
 }
