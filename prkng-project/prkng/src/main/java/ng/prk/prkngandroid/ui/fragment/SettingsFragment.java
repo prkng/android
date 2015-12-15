910c5ad388679c9d2f3f56f4793c7861d694d148
Mudar Noufal <mn@mudar.ca>
2015-12-15 08:55:45 -0500

    Settings login

--- prkng-project/prkng/src/main/java/ng/prk/prkngandroid/ui/fragment/SettingsFragment.java
+++ prkng-project/prkng/src/main/java/ng/prk/prkngandroid/ui/fragment/SettingsFragment.java
@@ -1,14 +1,24 @@
 package ng.prk.prkngandroid.ui.fragment;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 
 import ng.prk.prkngandroid.Const;
 import ng.prk.prkngandroid.R;
+import ng.prk.prkngandroid.ui.activity.LoginEmailActivity;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements
+        Const.PrefsNames,
+        SharedPreferences.OnSharedPreferenceChangeListener,
+        Preference.OnPreferenceClickListener {
+
+    private SharedPreferences mPrefs;
+    private Preference pToggleLogin;
+
 
     public static SettingsFragment newInstance() {
         return new SettingsFragment();
@@ -23,5 +33,68 @@ public class SettingsFragment extends PreferenceFragment {
         pm.setSharedPreferencesMode(Context.MODE_PRIVATE);
 
         addPreferencesFromResource(R.xml.prefs_settings);
+
+        mPrefs = pm.getSharedPreferences();
+        pToggleLogin = findPreference(TOGGLE_LOGIN);
+
+        // Listeners
+        pToggleLogin.setOnPreferenceClickListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        /**
+         * Set up a listener whenever a key changes
+         */
+        if (mPrefs!= null) {
+            mPrefs.registerOnSharedPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        /**
+         * Remove the listener onPause
+         */
+        if (mPrefs != null) {
+            mPrefs.unregisterOnSharedPreferenceChangeListener(this);
+        }
+    }
+
+
+    /**
+     * Implements OnPreferenceClickListener
+     *
+     * @param preference
+     * @return
+     */
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        final String key = preference.getKey();
+        if (key == null) {
+            return false;
+        }
+
+        if (TOGGLE_LOGIN.equals(key)) {
+            startActivity(LoginEmailActivity.newIntent(getActivity()));
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Implements OnSharedPreferenceChangeListener
+     *
+     * @param sharedPreferences
+     * @param key
+     */
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
     }
 }
