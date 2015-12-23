39aafc7c86b56808ff42a4231007e82f56bbebb5
Mudar Noufal <mn@mudar.ca>
2015-12-23 09:51:26 -0500

    PasswordReset, show confirmation message

--- prkng-project/prkng/src/main/java/ng/prk/prkngandroid/ui/activity/LoginForgotPasswordActivity.java
+++ prkng-project/prkng/src/main/java/ng/prk/prkngandroid/ui/activity/LoginForgotPasswordActivity.java
@@ -2,11 +2,13 @@ package ng.prk.prkngandroid.ui.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.TextView;
 
@@ -15,7 +17,8 @@ import com.rengwuxian.materialedittext.validation.METValidator;
 
 import ng.prk.prkngandroid.Const;
 import ng.prk.prkngandroid.R;
-import ng.prk.prkngandroid.ui.thread.PasswordResetTask;
+import ng.prk.prkngandroid.io.ApiClient;
+import ng.prk.prkngandroid.io.PrkngApiError;
 import ng.prk.prkngandroid.util.AuthValidation;
 import ng.prk.prkngandroid.util.EditTextUtils;
 
@@ -24,6 +27,8 @@ public class LoginForgotPasswordActivity extends AppCompatActivity implements
     private static final String TAG = "ForgotPassActivity";
 
     private MaterialEditText vEmail;
+    private ViewGroup vForm;
+    private TextView vMessage;
 
     public static Intent newIntent(Context context, String email) {
         final Intent intent = new Intent(context, LoginForgotPasswordActivity.class);
@@ -44,6 +49,8 @@ public class LoginForgotPasswordActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_forgot_password);
 
         vEmail = (MaterialEditText) findViewById(R.id.email);
+        vForm = (ViewGroup) findViewById(R.id.form_reset);
+        vMessage = (TextView) findViewById(R.id.confirm_message);
 
         final String email = getIntent().getStringExtra(Const.BundleKeys.EMAIL);
         vEmail.setText(email);
@@ -100,6 +107,42 @@ public class LoginForgotPasswordActivity extends AppCompatActivity implements
         if (vEmail.validate()) {
             final String email = EditTextUtils.getText(vEmail);
             new PasswordResetTask().execute(email);
+        }
+    }
+
+    private class PasswordResetTask extends AsyncTask<String, Void, Void> {
+
+        private PrkngApiError error = null;
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                final String email = params[0];
+                if (email != null) {
+                    ApiClient.resetUserPassword(
+                            ApiClient.getServiceLog(),
+                            email);
+                }
+            } catch (PrkngApiError e) {
+                error = e;
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if (error != null) {
+                error.showSnackbar(vForm);
+                return;
+            }
+
+            try {
+                vForm.setVisibility(View.GONE);
+                vMessage.setVisibility(View.VISIBLE);
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
