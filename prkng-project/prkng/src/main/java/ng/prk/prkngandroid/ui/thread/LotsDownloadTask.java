0f887240af99f8f4563ca445972dc2ad24ba3837
Mudar Noufal <mn@mudar.ca>
2015-12-30 16:13:29 -0500

    Map display custom lot markers

--- prkng-project/prkng/src/main/java/ng/prk/prkngandroid/ui/thread/LotsDownloadTask.java
+++ prkng-project/prkng/src/main/java/ng/prk/prkngandroid/ui/thread/LotsDownloadTask.java
@@ -6,16 +6,19 @@ import com.mapbox.mapboxsdk.views.MapView;
 
 import java.util.List;
 
+import ng.prk.prkngandroid.Const;
 import ng.prk.prkngandroid.io.ApiClient;
 import ng.prk.prkngandroid.io.PrkngApiError;
 import ng.prk.prkngandroid.io.PrkngService;
 import ng.prk.prkngandroid.model.GeoJSONFeatureProperties;
-import ng.prk.prkngandroid.model.ui.MapAssets;
+import ng.prk.prkngandroid.model.LotCurrentStatus;
 import ng.prk.prkngandroid.model.MapGeometry;
 import ng.prk.prkngandroid.model.PointsGeoJSON;
 import ng.prk.prkngandroid.model.PointsGeoJSONFeature;
 import ng.prk.prkngandroid.model.SpotsAnnotations;
+import ng.prk.prkngandroid.model.ui.MapAssets;
 import ng.prk.prkngandroid.ui.thread.base.PrkngDataDownloadTask;
+import ng.prk.prkngandroid.util.CalendarUtils;
 
 public class LotsDownloadTask extends PrkngDataDownloadTask {
     private final static String TAG = "LotsTask";
@@ -51,17 +54,28 @@ public class LotsDownloadTask extends PrkngDataDownloadTask {
                         mapGeometry.getRadius()
                 );
 
+//                ImageView mImageOne =
+                final long now = CalendarUtils.todayMillis();
+
+
                 // Prepare map annotations: Markers only
                 final List<PointsGeoJSONFeature> lotsFeatures = lots.getFeatures();
+
+                final int bestPrice = getBestPrice(lotsFeatures, now);
+
                 for (PointsGeoJSONFeature feature : lotsFeatures) {
                     final GeoJSONFeatureProperties properties = feature.getProperties();
+
+                    final LotCurrentStatus status = feature.getProperties().getAgenda().getLotCurrentStatus(now);
+                    final int price = (status == null) ? Const.UNKNOWN_VALUE : status.getMainPriceRounded();
+                    final int type = (status == null) ? Const.BusinnessHourType.CLOSED : Const.BusinnessHourType.OPEN;
 
                     final List<Double> latLng = feature.getGeometry().getCoordinates();
                     final MarkerOptions markerOptions = new MarkerOptions()
                             .position(new LatLng(new LatLng(latLng.get(1), latLng.get(0))))
                             .title(properties.getAddress())
-                            .snippet(feature.getId());
-//                            .icon(properties.isTypePaid() ? mapAssets.getMarkerIconPaid() : mapAssets.getMarkerIconFree());
+                            .snippet(feature.getId())
+                            .icon(mapAssets.getLotMarkerIcon(price, type, price == bestPrice));
 
                     spotsAnnotations.addMarker(feature.getId(), markerOptions);
                 }
@@ -73,6 +87,19 @@ public class LotsDownloadTask extends PrkngDataDownloadTask {
         }
 
         return spotsAnnotations;
+    }
+
+    private static int getBestPrice(List<PointsGeoJSONFeature> lotsFeatures, long now) {
+        int best = Integer.MAX_VALUE;
+
+        for (PointsGeoJSONFeature feature : lotsFeatures) {
+            final LotCurrentStatus status = feature.getProperties().getAgenda().getLotCurrentStatus(now);
+            if (status != null) {
+                best = Math.min(best, status.getMainPriceRounded());
+            }
+        }
+
+        return best;
     }
 
 }
