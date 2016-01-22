5b8ac88cae9b5aa021656030902bc642537d3e44
Mudar Noufal <mn@mudar.ca>
2016-01-22 15:56:40 -0500

    Tabs layout
    related to #36

--- prkng-project/prkng/src/main/java/ng/prk/prkngandroid/ui/view/MapTabLayout.java
+++ prkng-project/prkng/src/main/java/ng/prk/prkngandroid/ui/view/MapTabLayout.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Typeface;
 import android.support.design.widget.TabLayout;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -12,27 +11,25 @@ import android.widget.TextView;
 import ng.prk.prkngandroid.Const;
 import ng.prk.prkngandroid.util.TypefaceHelper;
 
-public class IntroTabLayout extends TabLayout {
+public class MapTabLayout extends TabLayout {
 
     private Typeface typeface;
-    public IntroTabLayout(Context context) {
+    public MapTabLayout(Context context) {
         this(context, null);
     }
 
-    public IntroTabLayout(Context context, AttributeSet attrs) {
+    public MapTabLayout(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
     }
 
-    public IntroTabLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+    public MapTabLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-Log.v("IntroTabLayout", "constructor");
         typeface = TypefaceHelper.getTypeface(context, Const.TypeFaces.REGULAR);
     }
 
     @Override
     public void addTab(Tab tab) {
         super.addTab(tab);
-        Log.v("addTab", "constructor");
 
         ViewGroup mainView = (ViewGroup) getChildAt(0);
         ViewGroup tabView = (ViewGroup) mainView.getChildAt(tab.getPosition());
