506574fa8ca4544b78e446d1bfe09e354aa5a3f7
Mudar Noufal <mn@mudar.ca>
2016-01-26 12:29:50 -0500

    Remove invisible markers on LineString
    related to #51

--- prkng-project/prkng/src/main/java/ng/prk/prkngandroid/ui/thread/SpotsDownloadTask.java
+++ prkng-project/prkng/src/main/java/ng/prk/prkngandroid/ui/thread/SpotsDownloadTask.java
@@ -75,22 +75,6 @@ public class SpotsDownloadTask extends PrkngDataDownloadTask {
                             .color(properties.isTypePaid() ? mapAssets.getLineColorPaid() : mapAssets.getLineColorFree());
                     spotsAnnotations.addPolyline(feature.getId(), polylineOptions);
 
-                    // Add first/last points as invisible buttons too
-                    // For z-index, must be added before the visible ones
-                    final MarkerOptions firstMarkerOptions = new MarkerOptions()
-                            .position(pointsArray[0])
-                            .title(properties.getWayName())
-                            .snippet(feature.getId());
-                    firstMarkerOptions.icon(mapAssets.getMarkerIconTransparent());
-                    spotsAnnotations.addMarker(feature.getId(), firstMarkerOptions);
-
-                    final MarkerOptions lastMarkerOptions = new MarkerOptions()
-                            .position(pointsArray[pointsArray.length - 1])
-                            .title(properties.getWayName())
-                            .snippet(feature.getId());
-                    lastMarkerOptions.icon(mapAssets.getMarkerIconTransparent());
-                    spotsAnnotations.addMarker(feature.getId(), lastMarkerOptions);
-
                     // Add the visible buttons
                     List<LatLng> buttons = properties.getButtonLocations();
                     for (LatLng buttonLatLng : buttons) {
